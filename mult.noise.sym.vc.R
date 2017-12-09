c53b384a4aa4dea340bcaab0b685c63d7b0520f4
elundgre <elundgre@usc.edu>
2017-12-08 18:49:49 -0800

    var coal noise script

---
+++ mult.noise.sym.vc.R
@@ -0,0 +1,19 @@
+#different noise levels, symmetric, unknown if coalescence rates are same everywhere
+
+require(parallel)
+require(gene.flow.inference)
+
+#file path for output
+#out_path <- "~/Dropbox/code/gene-flow-inference/plots/"
+#or use current path
+out_path <- getwd()
+
+#number of replicates
+reps <- 25
+
+noise_values <- c(rep(1/1000,reps),rep(1/200,reps),rep(1/100,reps),rep(1/50,reps))
+seeds <- sample(1000000000,reps)
+
+time_m <- system.time(mult <- mcmapply(mult.small,noise=noise_values,gam=1,const_coal=FALSE,symmetric=TRUE,
+                                       seed=rep(seeds,4),mc.cores=6,SIMPLIFY=FALSE))
+save.image("mult_noise_sym_vc.RData")
